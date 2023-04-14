aaf21e964b73095fb613e6ae4ae0ad580c0ee1c8
jjbotinam <jjbotinam@unal.edu.co>
2023-04-14 13:42:16 -0500

    respuesta

--- pregunta.py
+++ pregunta.py
@@ -1,35 +1,42 @@
 """
 Limpieza de datos usando Pandas
------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
-correctamente. Tenga en cuenta datos faltantes y duplicados.
-
+Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
-from datetime import datetime
 
 def clean_data():
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df.drop(df.columns[0], axis=1, inplace=True)
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace('[\$,]', '', regex=True)
-    df["monto_del_credito"] = df["monto_del_credito"].astype(str).str.split(".", expand=True)[0]
-    df['idea_negocio'] = df['idea_negocio'].str.replace('-', ' ')
-    df['línea_credito'] = df['línea_credito'].str.replace('-', ' ')
-    df['barrio'] = df['barrio'].str.replace('-', ' ')
-    df['idea_negocio'] = df['idea_negocio'].str.replace('_', ' ')
-    df['línea_credito'] = df['línea_credito'].str.replace('_', ' ')
-    df['barrio'] = df['barrio'].str.replace('_', ' ')
-    def formato_fecha(date_str):
-        try:
-            date_obj = datetime.strptime(date_str, '%Y/%m/%d') 
-        except ValueError:
-            date_obj = datetime.strptime(date_str, '%d/%m/%Y')  
-            date_obj = datetime.strptime(date_obj.strftime('%Y/%m/%d'), '%Y/%m/%d')  
-        return date_obj
-    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(formato_fecha)
-    df = df.applymap(lambda x: x.lower() if type(x) == str else x)
-    df = df.apply(lambda x: x.str.strip() if x.name != 'barrio' and x.dtype == 'O' else x)
-    df = df.dropna()
-    df = df.drop_duplicates()
+
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df.reset_index(inplace=True,drop=True)
+    
+    # Date check  
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
+    
+    df.dropna(axis='index',inplace=True)
+    df.drop_duplicates(inplace=True)
+
+    # Lowercase check
+    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
+    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
+    df['barrio'] = df['barrio'].str.lower().astype(str)
+    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
+     
+    # Whitespaces, hyphen and underscore checks
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
+    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
+
+    # Money check
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
+    
+    # Others
+    # df['línea_credito'] = df['línea_credito'].str.replace('soli diaria','solidaria')
+    # df['idea_negocio'] = df['idea_negocio'].str.replace('[^a-zA-Z0-9 \n\.]', ' ',regex=True).str.replace('organizaci n y','organizacion y').str.replace('pa alera','panalera')
+    # df['barrio'] =  df['barrio'].str.replace('[^a-zA-Z0-9 \n\.]',' ',regex=True).str.replace('bel n','belen').str.replace('san jos  de la monta a','san jose de la montana').str.replace('antonio nari  o','antonio narino').str.replace('antonio nari o','antonio narino')
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis='index',inplace=True)
     return df
